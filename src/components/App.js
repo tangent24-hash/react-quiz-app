b9983235cde585e17967710cd6fa33364eac041c
Sohanur <sohanur471@gmail.com>
2021-12-10 11:25:26 +0600

    homepage layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <div>Hi there!</div>
+      <Layout>
+        <Home />
+      </Layout>
     </div>
   );
 }

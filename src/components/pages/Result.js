9db97a9cb8af22e4308d829996ff553a11280663
Sohanur <sohanur471@gmail.com>
2021-12-14 22:25:55 +0600

    Result page lesson-6

---
+++ src/components/pages/Result.js
@@ -0,0 +1,11 @@
+import Analysis from "../Analysis.js";
+import Summary from "../Summary.js";
+
+export default function Result() {
+  return (
+    <>
+      <Summary />
+      <Analysis />
+    </>
+  );
+}
